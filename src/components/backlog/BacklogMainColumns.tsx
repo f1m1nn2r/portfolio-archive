1c842b29016b024545314d3ad8f17545b76dbb76
김민중 <f1minn2r@naver.com>
2026-01-28 19:42:31 +0900

    feat: 백로그 메인 페이지 구축 및 사용자용 리스트 노출

---
+++ src/components/backlog/BacklogMainColumns.tsx
@@ -0,0 +1,66 @@
+import { Backlog, Epic } from "@/types/admin/backlog";
+import { AdminTableColumn } from "@/types/admin/table";
+import { Badge } from "@/components/common/Badge";
+import { Icon } from "@/components/common/Icon";
+
+export const BacklogMainColumns = (
+  allEpics: readonly Epic[],
+  currentPage: number,
+): AdminTableColumn<Backlog>[] => [
+  {
+    label: "NO",
+    width: "w-16",
+    center: true,
+    renderCell: (_, index) => {
+      const itemsPerPage = 20;
+      return <span>{(currentPage - 1) * itemsPerPage + index + 1}</span>;
+    },
+  },
+  {
+    label: "화면",
+    width: "w-[150px]",
+    renderCell: (item) => <span className="font-medium">{item.screen}</span>,
+  },
+  {
+    label: "Epic",
+    width: "w-[180px]",
+    renderCell: (item) => (
+      <div className="flex flex-wrap gap-1">
+        {item.epic_ids?.map((id) => {
+          const epic = allEpics.find((e) => e.id === id);
+          return epic ? (
+            <Badge key={id} backgroundColor={epic.color}>
+              {epic.label}
+            </Badge>
+          ) : null;
+        })}
+      </div>
+    ),
+  },
+  {
+    label: "기능",
+    width: "w-[250px]",
+    renderCell: (item) => (
+      <span className="truncate block">{item.feature}</span>
+    ),
+  },
+  {
+    label: "설명",
+    width: "w-[350px]",
+    renderCell: (item) => (
+      <span className="whitespace-normal break-all" title={item.description}>
+        {item.description || "-"}
+      </span>
+    ),
+  },
+  {
+    label: "상태",
+    center: true,
+    width: "w-[100px]",
+    renderCell: (item) => (
+      <div className="flex justify-center">
+        <Icon type={item.is_done ? "checkboxChecked" : "checkbox"} size={24} />
+      </div>
+    ),
+  },
+];
